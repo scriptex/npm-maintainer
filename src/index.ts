f4ae894bf10273cc25ebb3b318eb1899f6225a5f
Atanas Atanasov <scriptex.bg@gmail.com>
2020-11-18 17:28:27 +0200

    Successfully fetch packages and data

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,19 @@ type ApiResponse = {
 };
 
 class NPMUserStats extends HTMLElement {
-	private ENDPOINT = 'https://api.npms.io/v2/search';
-
+	private SIZE = 250;
+	private API_URL = 'https://api.npms.io/v2';
 	private data: ApiResponse = {
 		total: 0,
 		results: []
 	};
-
-	private styleEl: HTMLStyleElement = document.createElement('style');
 	private template: HTMLTemplateElement = document.createElement('template');
 
 	private error = false;
 	private loading = true;
 
 	static get observedAttributes(): string[] {
-		return ['user', 'style'];
+		return ['user'];
 	}
 
 	constructor() {
@@ -35,39 +33,72 @@ class NPMUserStats extends HTMLElement {
 		this.render();
 	}
 
-	private async fetch(user: string, from: number): Promise<ApiResponse> {
-		return fetch(`${this.ENDPOINT}?q=maintainer:${user}&size=250&from=${from}`)
+	private async fetch(args: string, options?: RequestInit): Promise<any> {
+		return fetch(`${this.API_URL}${args}`, options)
 			.then((r: Response) => r.json())
 			.catch(() => {
 				this.error = true;
 			});
 	}
 
-	private async fetchData(user: string): Promise<ApiResponse> {
+	private async fetchData(user: string): Promise<any> {
 		let from = 0;
 		let total = 0;
 
 		const limit = 250;
-		const results = [];
+		const items: any[] = [];
 
 		if (from === 0) {
-			const response = await this.fetch(user, from);
+			const response = await this.fetch(`/search?q=maintainer:${user}&size=250&from=${from}`);
 
 			if (response.total <= limit) {
-				return response;
+				return await this.build(response.results, response.total, user, from);
 			}
 
 			total = response.total;
-			results.push(...response.results);
+			items.push(...response.results);
 		}
 
-		while (results.length < total) {
+		return await this.build(items, total, user, from);
+	}
+
+	private async build(items: any, total: any, user: string, from: number) {
+		while (items.length < total) {
 			from += 250;
 
-			const next = await this.fetch(user, from);
+			const next = await this.fetch(`/search?q=maintainer:${user}&size=250&from=${from}`);
 
-			results.push(...next.results);
+			items.push(...next.results);
 		}
+
+		const copy = [...items];
+
+		const namesArray = new Array(Math.ceil(items.length / this.SIZE)).fill('').map(_ => copy.splice(0, this.SIZE));
+
+		console.log(namesArray);
+
+		const packagesDetails = namesArray.map(items => {
+			return this.fetch(`/package/mget`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(items.map(item => item.package.name))
+			});
+		});
+
+		const details = (await Promise.all(packagesDetails)).reduce((sum, current) => {
+			return { ...sum, ...current };
+		}, {});
+
+		const results = Object.keys(details).reduce((result, key) => {
+			const match = items.find((item: any) => item.package.name === key);
+
+			result.push({
+				...match,
+				...details[key]
+			});
+
+			return result;
+		}, [] as any);
 
 		this.loading = false;
 
@@ -88,12 +119,37 @@ class NPMUserStats extends HTMLElement {
 			? 'Error fetching data.'
 			: `<div>${this.renderStats()}</div>`;
 
-		shadowRoot.appendChild(this.styleEl.cloneNode(true));
 		shadowRoot.appendChild(this.template.content.cloneNode(true));
 	}
 
 	private renderStats(): string {
-		return `total: ${this.data.total}`;
+		const cols = ['Name', 'Popularity', 'Downloads', 'Interest', 'Dependents'];
+		const names = this.data.results.map(({ package: { name } }) => name);
+
+		console.log(this.data);
+
+		return `
+<table>
+	<thead>
+		<tr>
+			${cols.map(name => `<th>${name}</th>`).join('\n')}
+		</tr>
+	</thead>
+	<tbody>
+	${names
+		.map(
+			name => `
+		<tr>
+			<td>
+				<a href="${`https://npmjs.org/package/${name}`}" target="_blank">${name}</a>
+			</td>
+		</tr>
+	`
+		)
+		.join('\n')}
+	</tbody>
+	</table>
+`;
 	}
 }
 
